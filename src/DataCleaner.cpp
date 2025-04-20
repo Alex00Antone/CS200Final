db0767211dfbeed03e9fa5d91f4c17de051d0f66
Alex00Antone <alexjantone@gmail.com>
2025-04-19 22:06:35 -0600

    initial 4

--- src/DataCleaner.cpp
+++ src/DataCleaner.cpp
@@ -1,1 +1,18 @@
 #include "DataCleaner.h"
+
+
+
+    //creates the dataCleaning object with a file member
+    DataCleaner::DataCleaner(){
+
+    }
+
+    //returns file member
+    vector <vector<string>> DataCleaner::getFile() const{
+
+    }
+
+    //modifies file member to remove null values and duplicate data entries
+    void DataCleaner::cleanData(){
+
+    }
